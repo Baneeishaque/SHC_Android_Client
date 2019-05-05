d99a8cfe30256aa93f30aba219f8812d81de762d
Banee Ishaque K <baneeishaque@gmail.com>
2019-05-05 10:13:36 +0530

    Runtime Permission

--- app/src/main/java/ndk/ccetv_group8/shc/DiseasePredictionSuccess.java
+++ app/src/main/java/ndk/ccetv_group8/shc/DiseasePredictionSuccess.java
@@ -1,8 +1,18 @@
 package ndk.ccetv_group8.shc;
 
+import android.content.pm.PackageManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.util.Pair;
 
+import ndk.utils_android16.Snackbar_Utils;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class DiseasePredictionSuccess extends TextWithButtonsActivity {
+    private static final int PERMISSION_REQUEST_CODE = 0;
 
     //TODO : Centralise Layout
 
@@ -23,6 +33,33 @@ public class DiseasePredictionSuccess extends TextWithButtonsActivity {
 
     @Override
     protected Pair[] configureButtonsWithClickEvents() {
-        return new Pair[]{new Pair<>(getResources().getString(R.string.proceed), ButtonUtils.getStartActivityButtonEvent(activity_context, D5.class)), new Pair<>(getResources().getString(R.string.try_again), ButtonUtils.getBackButtonEvent(this)), new Pair<>(getResources().getString(R.string.no_thanks), ButtonUtils.getButtonEvent(() -> ActivityUtils.toHomeActivityWithConfirmation(activity_context)))};
+        return new Pair[]{new Pair<>(getResources().getString(R.string.proceed), ButtonUtils.getButtonEvent(new ButtonUtils.FurtherActions() {
+            @Override
+            public void configureFurtherActions() {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION) != 0) {
+                    ActivityCompat.requestPermissions((AppCompatActivity) activity_context, new String[]{ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+                } else {
+                    ndk.utils_android1.ActivityUtils.start_activity(activity_context, D5.class);
+                }
+            }
+        })), new Pair<>(getResources().getString(R.string.try_again), ButtonUtils.getBackButtonEvent(this)), new Pair<>(getResources().getString(R.string.no_thanks), ButtonUtils.getButtonEvent(() -> ActivityUtils.toHomeActivityWithConfirmation(activity_context)))};
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+                boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                if (locationAccepted) {
+                    Snackbar_Utils.display_Short_no_FAB_success_bottom_SnackBar(activity_context, "Permission Granted, Thanks.");
+                    ndk.utils_android1.ActivityUtils.start_activity(activity_context, D5.class);
+                } else {
+                    Snackbar_Utils.display_Short_no_FAB_error_bottom_SnackBar(activity_context, "Please allow location access for better doctor search results.");
+                }
+            }
+        }
+    }
+
 }
